22ac24cf4e263ed5444282e070baf14fa1e2f41a
BuildTools <unconfigured@null.spigotmc.org>
2021-09-19 18:57:50 -0500

    Initial Commit

---
+++ homework/utils.py
@@ -0,0 +1,43 @@
+from PIL import Image
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+LABEL_NAMES = ['background', 'kart', 'pickup', 'nitro', 'bomb', 'projectile']
+
+
+class SuperTuxDataset(Dataset):
+    def __init__(self, dataset_path):
+        """
+        Your code here
+        Hint: Use the python csv library to parse labels.csv
+
+        WARNING: Do not perform data normalization here. 
+        """
+        self.data = torchvision.datasets.CIFAR10(root=dataset_path, train=True, 
+            download=True, transform=None)
+
+
+    def __len__(self):
+        """
+        Your code here
+        """
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        """
+        Your code here
+        return a tuple: img, label
+        """
+        return self.data[idx]
+
+
+def load_data(dataset_path, num_workers=0, batch_size=128):
+    dataset = SuperTuxDataset(dataset_path)
+    return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=False)
+
+
+def accuracy(outputs, labels):
+    outputs_idx = outputs.max(1)[1].type_as(labels)
+    return outputs_idx.eq(labels).float().mean()
